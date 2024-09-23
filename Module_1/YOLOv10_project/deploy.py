9836439765924bd11c4d14a8f5925d2a20cbf232
Do Huy Duc <dohuyduc.work@gmail.com>
2024-09-23 10:58:28 +0700

    Save local changes before merging develop

--- Module_1/YOLOv10_project/deploy.py
+++ Module_1/YOLOv10_project/deploy.py
@@ -4,7 +4,10 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 from ultralytics import YOLOv10
+<<<<<<< Updated upstream
 import torch
+=======
+>>>>>>> Stashed changes
 
 MODEL_PATH = "./models/best.pt"
 
@@ -12,7 +15,11 @@ MODEL_PATH = "./models/best.pt"
 def process_image(image):
     model = YOLOv10(MODEL_PATH)
     result = model(image)[0]
+<<<<<<< Updated upstream
     result.save('./output.png')
+=======
+    result.save('./predicts/output.png')
+>>>>>>> Stashed changes
 
 
 def main():
@@ -24,7 +31,11 @@ def main():
         image = Image.open(file)
         image = np.array(image)
         process_image(image)
+<<<<<<< Updated upstream
         predict_image = cv2.imread('./output.png')
+=======
+        predict_image = cv2.imread('./predicts/output.png')
+>>>>>>> Stashed changes
         st.image(predict_image, caption="Predict Image")
 
 
